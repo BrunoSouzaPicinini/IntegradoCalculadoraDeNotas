e17bc56fe15ac8fa9be0c2c1863caa25ce0c800c
BrunoSouzaPicinini <brunosouzapicinini@gmail.com>
2017-03-30 11:32:19 -0300

    SingUpActivity - Inicial

---
+++ app/src/main/java/br/com/bspicinini/calculadoradenotas/activitys/SingUpActivity.java
@@ -0,0 +1,16 @@
+package br.com.bspicinini.calculadoradenotas.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.bspicinini.calculadoradenotas.R;
+
+
+public class SingUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+    }
+}
