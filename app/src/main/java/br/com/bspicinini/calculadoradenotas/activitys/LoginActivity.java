62bd2a1b1caa37c820f8699cbac42c2a68424208
BrunoSouzaPicinini <brunosouzapicinini@gmail.com>
2017-03-30 13:44:38 -0300

    SingUpActivity - Inicial

--- app/src/main/java/br/com/bspicinini/calculadoradenotas/activitys/LoginActivity.java
+++ app/src/main/java/br/com/bspicinini/calculadoradenotas/activitys/LoginActivity.java
@@ -1,10 +1,12 @@
 package br.com.bspicinini.calculadoradenotas.activitys;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
 import android.view.View;
 import android.widget.EditText;
 
@@ -18,13 +20,27 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
+    public void onClickLoginUp(View v){
+            EditText edtUsuario = (EditText) findViewById(R.id.edtLogin);
+            String  usuario = edtUsuario.getText().toString();
+            EditText edtSenha = (EditText) findViewById(R.id.edtSenha);
+            Intent intent = new Intent(getApplicationContext(), ListagemActivity.class);
+            intent.putExtra("Login",usuario);
+            startActivity(intent);
+
+
+    }
+
 
     public void onClickSingUp(View v){
-            if(v.getId() == R.id.login_in_button){
-                EditText edtUsuario = (EditText) findViewById(R.id.edtLogin);
-                EditText edtSenha = (EditText) findViewById(R.id.edtSenha);
+            EditText edtUsuario = (EditText) findViewById(R.id.edtLogin);
+            String  usuario = edtUsuario.getText().toString();
+            EditText edtSenha = (EditText) findViewById(R.id.edtSenha);
 
-            }
+            Intent intent = new Intent(getApplicationContext(), SingUpActivity.class);
+            intent.putExtra("Login",usuario);
+            startActivity(intent);
+
 
     }
 
--- app/src/main/java/br/com/bspicinini/calculadoradenotas/activitys/SingUpActivity.java
+++ app/src/main/java/br/com/bspicinini/calculadoradenotas/activitys/SingUpActivity.java
@@ -2,6 +2,8 @@ package br.com.bspicinini.calculadoradenotas.activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import br.com.bspicinini.calculadoradenotas.R;
 
@@ -12,5 +14,14 @@ public class SingUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
+    }
+
+    public void onClickSingUp(View v){
+        if(v.getId() == R.id.login_in_button){
+            EditText edtUsuario = (EditText) findViewById(R.id.edtLogin);
+            EditText edtSenha = (EditText) findViewById(R.id.edtSenha);
+
+        }
+
     }
 }
